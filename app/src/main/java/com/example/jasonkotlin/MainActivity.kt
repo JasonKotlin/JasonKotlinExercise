346ab40ec866d4c7a2924e0a52d1538557423701
JasonYang <jason.yang@mitake.com.tw>
2021-03-07 00:06:25 +0800

    SecondTest

--- app/src/main/java/com/example/jasonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/jasonkotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.jasonkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun test1(view: View) {
+        val i : Intent = Intent(this, SecondActivity::class.java).apply {
+            putExtra("key", "goSecond")
+        }
+
+        startActivity(i)
     }
 }
